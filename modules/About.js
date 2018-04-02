2d6e9e79db7eaa5bed55466a5924852af6c80148
alessandrochetta <alessandrochetta@gmail.com>
2018-04-02 15:36:19 -0500

    first commit

---
+++ modules/About.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return <div>
+        <img src="http://www.resumetem.com/wp-content/uploads/2017/10/sample-depositphotos_35186549-stock-photo-sample-grunge-red-round-stamp.jpg" height="300px" width="300px"/>
+        <img src="http://www.joshuacasper.com/contents/uploads/joshua-casper-samples-free.jpg" height="300px" width="300px"/>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Simple_random_sampling.PNG/300px-Simple_random_sampling.PNG" height="300px" width="300px"/>
+        <img src="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX11405548.jpg" height="300px" width="300px"/>
+        <img src="https://cdn.xl.thumbs.canstockphoto.com/sample-blue-square-grungy-stamp-isolated-on-white-background-drawing_csp20635443.jpg" height="300px" width="300px"/>
+        <img src="https://www.myfico.com/Images/sample_overlay.gif" height="300px" width="300px"/>
+        <img src="https://previews.123rf.com/images/carmenbobo/carmenbobo1312/carmenbobo131200063/24308511-stamp-with-word-sample-inside-vector-illustration.jpg" height="300px" width="300px"/>
+        <img src="http://3.bp.blogspot.com/-8cQhL1rxNU4/VOsaEN4ofdI/AAAAAAAAHaA/11tGkd7TXEU/s1600/sample.jpg" height="300px" width="300px"/>
+        <img src="http://i.imgur.com/I86rTVl.jpg" height="300px" width="300px"/>
+        <img src="https://comps.canstockphoto.com/sample-image_csp27057271.jpg" height="300px" width="300px"/>
+        <img src="https://thumbs.dreamstime.com/z/sample-stamp-white-background-sign-90532936.jpg" height="300px" width="300px"/>
+        <img src="http://tfsassets.azureedge.net/sampletry.jpg" height="300px" width="300px"/>
+        <img src="https://keydifferences.com/wp-content/uploads/2016/06/population-vs-sample2.jpg" height="300px" width="300px"/>
+        <img src="https://thumb9.shutterstock.com/display_pic_with_logo/436114/274833056/stock-vector-sample-grunge-retro-red-isolated-ribbon-stamp-sample-stamp-sample-sample-sign-274833056.jpg" height="300px" width="300px"/>
+        <img src="http://www.alittlethingcalledlove.co.uk/wp-content/uploads/2015/01/Sample-image.jpg" height="300px" width="300px"/>
+        <img src="http://orperry.com/sample/wp-content/uploads/2015/12/sample-logo.png" height="300px" width="300px"/>
+      </div>
+  }
+})
---
+++ modules/Home.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return <div><img src="test.jpg" height="300px" width="300px"/></div>
+  }
+})
